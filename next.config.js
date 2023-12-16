cd9dfa2bd4ba163713b4fc97dcc6951648e725ee
Stepan Korshukov <korshukovstepan@gmail.com>
2023-12-16 08:43:07 +0300

    feat:Add authentication, internal CMS for sellers, and Stripe integration
    This commit adds the following features to the app:
    - Authentication functionality
    - Internal CMS for sellers to add products
    - Stripe integration for payment processing
    
    These changes are now ready for testing and deployment.

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "http",
+				hostname: "localhost",
+			},
+			{
+				protocol: "https",
+				hostname: "hippoicons.korshukovstepan.dev",
+			},
+		],
+	},
+};
+
+module.exports = nextConfig;
