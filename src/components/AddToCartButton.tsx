60c723c6cdee062ea0c2913ae708f48118889a6c
Stepan Korshukov <korshukovstepan@gmail.com>
2023-12-19 15:18:12 +0300

    feat: Remove SetTimeout from AddToCartButton component

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Button } from './ui/button'
 import { useCart } from '@/hooks/use-cart'
 import { Product } from '@/payload-types'
@@ -11,13 +11,6 @@ const AddToCartButton = ({
 }) => {
   const { addItem, items } = useCart();
   const [isSuccess, setIsSuccess] = useState<boolean>(false)
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setIsSuccess(false)
-    }, 2000)
-    return () => clearTimeout(timeout)
-  }, [isSuccess])
 
   const handleAddToCart = () => {
     const isProductInCart = items.some((item) => item.product.id === product.id);
